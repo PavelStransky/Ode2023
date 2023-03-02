48d426fd1a10cd4346c00f3fec8ccb09d5c1473a
Pavel Stránský <github@pavelstransky.cz>
2023-03-02 16:51:29 +0100

    docstrings

--- ode.py
+++ ode.py
@@ -1,9 +1,21 @@
 def euler_1(model, y, t, dt):
+    """ First-order Euler method """
     y1 = y + model(y, t) * dt
     t1 = t + dt
     return y1, t1
 
 def ode_solve(model, initial_condition, integrator, dt, maxt):
+    """ Numerical solution of a differential equation by the specified integrator.
+
+        Arguments:
+        model -- a function returning the right-hand side of the ODE
+        dt -- step size
+        maxt -- integration performed from t=0 to t=maxt
+
+        Returns:
+        list with solution [y0, y1, y2, ...]
+        list with times [t0, t1, y2, ...]
+    """
     y = initial_condition
     ys = [y]
     
--- relaxation.py
+++ relaxation.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import ode
 
 def relaxation(y, t):
+    """ Derivatives for the relaxing system """
     return -y
 
 ys, ts = ode.ode_solve(relaxation, 1, integrator=ode.euler_1, dt=0.01, maxt=10)
